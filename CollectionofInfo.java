a6f8c8440657439f66a91bb29621c22440c8bfdf
JoaoPaulo32 <107009187+JoaoPaulo32@users.noreply.github.com>
2022-06-06 21:48:28 -0400

    Create CollectionofInfo.java

---
+++ CollectionofInfo.java
@@ -0,0 +1,15 @@
+pacote  br . com . aragia . iterador ;
+
+public  class  CollectionofInfo  implementa   Container {
+
+	public  int  idade [] = { 21 , 38 , 46 , 967 , 99 };
+	public  String  address [] = { "Céu" , "Coordenação" , "ProReitoria" , "Trabalho" , "Campo Grande" };
+	public  String  designation [] = { "Java Programmer and Content Writer" , "CEO" ,
+			"Professorora" , "Gerente" , "Escola/Faculdade" };
+
+	public  Iterator  getIterator () {
+		// stub do método TODO gerado automaticamente
+		retornar  nulo ;
+	}
+
+}
